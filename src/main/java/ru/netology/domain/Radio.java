c238aff33c6064943a42057e659bbdb1ef1b0948
Irina Rogova <buyersbirds@gmail.com>
2020-11-25 14:42:32 +0600

    added methods and tests

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,6 +1,63 @@
 package ru.netology.domain;
 
 public class Radio {
-    private int currentNumberRadioStation;
-    private int volumeRadioStation;
+    private int currentRadioStation;
+    private int firstRadioStation = 0;
+    private int lastRadioStation = 9;
+    private int currentSoundVolume;
+    private int minSoundVolume = 0;
+    private int maxSoundVolume = 10;
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation > lastRadioStation)
+            return;
+        if (currentRadioStation < firstRadioStation)
+            return;
+        this.currentRadioStation = currentRadioStation;
+    }
+
+    public void changeNextRadioStation() {
+        if (currentRadioStation == lastRadioStation) {
+            this.currentRadioStation = firstRadioStation;
+        }
+        else {
+            currentRadioStation++;
+        }
+    }
+
+    public void changePrevRadioStation() {
+        if (currentRadioStation == firstRadioStation) {
+            this.currentRadioStation = lastRadioStation;
+        } else {
+            currentRadioStation--;
+        }
+    }
+
+    public void increaseCurrentSoundVolume() {
+        if (currentSoundVolume < maxSoundVolume)
+            currentSoundVolume++;
+    }
+
+    public void decreaseCurrentSoundVolume() {
+        if (currentSoundVolume > minSoundVolume)
+            currentSoundVolume--;
+    }
+
+    public int getCurrentSoundVolume() {
+        return currentSoundVolume;
+    }
+
+    public void setCurrentSoundVolume(int currentSoundVolume) {
+        if (currentSoundVolume > maxSoundVolume)
+            return;
+        if (currentSoundVolume < minSoundVolume)
+            return;
+        this.currentSoundVolume = currentSoundVolume;
+    }
+
 }
+
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,7 +1,102 @@
 package ru.netology.domain;
 
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
+
+
+    @Test
+    public void shouldSetCurrentRadioStation() {
+        Radio radio = new Radio();
+
+        radio.setCurrentRadioStation(0);
+        assertEquals(0, radio.getCurrentRadioStation());
+
+        radio.setCurrentRadioStation(7);
+        assertEquals(7, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void shouldChangeNextRadioStationlast() {
+        Radio radio = new Radio();
+
+        radio.setCurrentRadioStation(9);
+        assertEquals(9, radio.getCurrentRadioStation());
+
+        radio.changeNextRadioStation();
+        assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void shouldChangePrevRadioStationFirst() {
+        Radio radio = new Radio();
+
+        radio.setCurrentRadioStation(0);
+        assertEquals(0, radio.getCurrentRadioStation());
+
+        radio.changePrevRadioStation();
+        assertEquals(9, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void shouldSetCurrentSoundVolume() {
+        Radio radio = new Radio();
+
+        radio.setCurrentSoundVolume(0);
+        assertEquals(0, radio.getCurrentSoundVolume());
+
+        radio.setCurrentSoundVolume(8);
+        assertEquals(8, radio.getCurrentSoundVolume());
+    }
+
+    @Test
+    public void shouldSetCurrentSoundVolumeOverMax() {
+        Radio radio = new Radio();
+
+        radio.setCurrentSoundVolume(0);
+        assertEquals(0, radio.getCurrentSoundVolume());
+
+        radio.setCurrentSoundVolume(11);
+        assertEquals(0, radio.getCurrentSoundVolume());
+    }
+
+    @Test
+    public void shouldSetCurrentSoundVolumeUnderMin() {
+        Radio radio = new Radio();
+
+        radio.setCurrentSoundVolume(0);
+        assertEquals(0, radio.getCurrentSoundVolume());
+
+        radio.setCurrentSoundVolume(-1);
+        assertEquals(0, radio.getCurrentSoundVolume());
+    }
+
+    @Test
+    public void shouldIncreaseCurrentSoundVolume() {
+        Radio radio = new Radio();
+
+        radio.setCurrentSoundVolume(8);
+        assertEquals(8, radio.getCurrentSoundVolume());
+
+        radio.increaseCurrentSoundVolume();
+        assertEquals(9, radio.getCurrentSoundVolume());
+    }
+
+    @Test
+    public void shouldDecreaseCurrentSoundVolume() {
+        Radio radio = new Radio();
+
+        radio.setCurrentSoundVolume(7);
+        assertEquals(7, radio.getCurrentSoundVolume());
+
+        radio.decreaseCurrentSoundVolume();
+        assertEquals(6, radio.getCurrentSoundVolume());
+    }
+
+
+
 
 }
